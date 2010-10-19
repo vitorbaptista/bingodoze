ee68e9bdd1b4303bc6c461793b20293a4395e767
Vítor Baptista <vitor@vitorbaptista.com>
2010-10-19 03:58:05 -0300

    Adicionando (e modificando script) pra usar cartas do Zé.
    Peguei os termos em http://twitpic.com/2yvfef.

--- script.js
+++ script.js
@@ -1,49 +1,31 @@
- $(document).ready(function(){
-   
-   var usedArray = new Array(76);
-   var baseArray = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
-   var number = 0;
-   var base = 0;
-   
-   init();
-   
-	function init(){
-		 for(var i = 0; i<24; i++){
-			fillCard(i);
-		 }
-	 }
-	  	 
-	 function fillCard(i){
-		 base = baseArray[i] * 15;
-		 number = base + Math.floor(Math.random()*15)+1;	
-		 
-		 
-		 if(usedArray[number] != true){
-			$('#cell'+i).html(number);
-				usedArray[number] = true;
-			}else{
-				fillCard(i);
-			}
-	 }
-	 
-	 function resetUsedNumbersArray(){
-		for(var j = 0; j < usedArray.length; j++){
-		usedArray[j] = false;
-		}	
-	 }
-	 
-	 
-	 $('#newCard').click(function(){
-		resetUsedNumbersArray();
-		init();
-	 });
-	 
-	 $('td').click(function(){
-		
-		var toggle = this.style;
-		toggle.backgroundColor = toggle.backgroundColor? "":"#333";
-		toggle.color = toggle.color? "":"#fff";
-	 });
+$(document).ready(function(){
+        var termos = ["A gente ouçe", "Trankilo", "Intiligenti", "Mais milhó", "Fardamentas",
+                      "Progama", "Problemas", "Combustivé", "Eu não sô homi di mintira", "Nós coloquemu",
+                      "Mais alto nivi", "Cássio", "PEC 300", "Isso não é verdade",
+                      "Seu tempo acabou, candidato", "Vacina contra as drogas", "Mais uma inverdade candidato",
+                      "Solucionamento", "Mudéstia", "Conexão 30", "Niuma", "Falei com Lula e Dilma...",
+                      "Me atrapalhou tudo aí", "Fardamentas"];
 
- });
+        fillCards();
+
+        function fillCards(){
+            var shuffle = function() { return 0.5 - Math.random() };
+            var termos_shuffled = termos.sort(shuffle);
+
+            for(var i = 0; i<termos.length; i++){
+                $('#cell'+i).html(termos_shuffled[i]);
+            }
+        }
+
+        $('#newCard').click(function(){
+                fillCards();
+        });
+
+        $('td').click(function(){
+                var toggle = this.style;
+                toggle.backgroundColor = toggle.backgroundColor? "":"#333";
+                toggle.color = toggle.color? "":"#fff";
+         });
+
+});
 
