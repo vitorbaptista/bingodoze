ada1d2fbd6229e6553df862920f485c81a44feab
Vítor Baptista <vitor@vitorbaptista.com>
2010-10-20 16:46:48 -0300

    Adiciona checagem do bingo.

--- script.js
+++ script.js
@@ -12,14 +12,51 @@ $(document).ready(function(){
             var shuffle = function() { return 0.5 - Math.random() };
             var termos_shuffled = termos.sort(shuffle);
 
-            for(var i = 0; i<termos.length; i++){
+            for(var i = 0; i<=24; i++){
+                // Pula a imagem de Maranhão
+                if (i == 12)
+                    continue;
+
                 $('#cell'+i).html(termos_shuffled[i]);
             }
         }
 
+        function checkBingo() {
+            var horizontal = false;
+            for (var i = 0; i <= 4; i++) {
+                var esta_horizontal = true;
+                for (var j = i; j <= 24; j += 5) {
+                    if (!($('#cell'+j).hasClass('ativo')))
+                        esta_horizontal = false;
+                }
+                horizontal = esta_horizontal;
+
+                if (horizontal)
+                    break;
+            }
+
+            var vertical = false;
+            for (var i = 0; i <= 20; i += 5) {
+                var esta_vertical = true;
+                for (var j = i; j <= i + 4; j++) {
+                    if (!($('#cell'+j).hasClass('ativo')))
+                        esta_vertical = false;
+                }
+
+                vertical = esta_vertical;
+                if (vertical)
+                    break;
+            }
+
+            if (vertical || horizontal)
+                $('#ganhou').html('GANHOU!');
+            else
+                $('#ganhou').html('');
+        }
+
         $('td').click(function(){
-                var toggle = this.style;
-                toggle.backgroundColor = toggle.backgroundColor? "":"#ff0000";
-                toggle.color = toggle.color? "":"#fff";
+                $(this).toggleClass('ativo');
+
+                checkBingo();
          });
 });
